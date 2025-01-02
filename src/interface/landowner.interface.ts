51c3eea10b23bbe0a032939f04d2c1758c7f003f
jaffasunny <jaffer.sunny125@gmail.com>
2025-01-02 16:10:26 +0400

    landowner paginate service

--- src/interface/landowner.interface.ts
+++ src/interface/landowner.interface.ts
@@ -1,5 +1,12 @@
-import mongoose from 'mongoose';
 import { IUser } from '../types/userTypes';
 import { IProperty } from './property.interface';
 
 export interface IAddLandownerParams extends IUser, IProperty {}
+
+export interface IlandownerAggregatePaginationServiceParams {
+  page: number;
+  limit: number;
+  search: string;
+  isArchived: boolean | null | string;
+  assigned: boolean | null | string;
+}
