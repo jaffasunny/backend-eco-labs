d76898f90e75d0cc672a33a148cb4566f5197da2
jaffasunny <jaffer.sunny125@gmail.com>
2025-01-02 11:38:11 +0400

    landowner api modified

--- src/middlewares/roles.middleware.ts
+++ src/middlewares/roles.middleware.ts
@@ -2,10 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { ROLES, RoleType } from '../constants.js';
 
-const roleCheck = (role: RoleType) => {
+const roleCheck = (role: RoleType[] | RoleType) => {
+  const checkRole = Array.isArray(role) ? role : [role];
+
   return (req: Request, res: Response, next: NextFunction) => {
     if (req.user.roles !== ROLES.ADMIN) {
-      if (req.user.roles !== role) {
+      if (!checkRole.includes(req.user.roles)) {
         return res
           .status(403)
           .json(new ApiResponse(403, 'Access denied, Incorrect role!'));
