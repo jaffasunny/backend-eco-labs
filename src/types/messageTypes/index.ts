286718ffc52ee34a14a2bd3614b29d2ff1ca3c87
jaffasunny <jaffer.sunny125@gmail.com>
2025-03-16 04:50:31 +0400

    removed

--- src/types/messageTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export interface IMessage {
-  sender: mongoose.Schema.Types.ObjectId;
-  content: string;
-  chat: mongoose.Schema.Types.ObjectId;
-  execPopulate(): any;
-}
